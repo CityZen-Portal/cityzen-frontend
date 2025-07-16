417579973f3f384d6b85e4be0e092889c57bfc0c
vishwa45k <717822f260@kce.ac.in>
2025-07-16 23:15:53 +0530

    feat:ScrollUpdate:NewsNavbarUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import CitizenLayout from "layouts/citizen";
@@ -7,17 +7,21 @@ import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
 
 import StaffLayout from "layouts/staff";
+import ScrollToTop from "views/citizen/news/components/ScrollToTop";
 const App = () => {
   
   return (
+    <>
+    <ScrollToTop/>
     <Routes>
+      
       <Route path="citizen/*" element={<CitizenLayout />} />
       <Route path="auth/*" element={<AuthLayout />} />
       <Route path="admin/*" element={<AdminLayout />} />
       <Route path="staff/*" element={<StaffLayout />} />
       <Route path="/*" element={<CitizenLayout />} />
     </Routes>
-  );
+</>  );
 };
 
 export default App;
