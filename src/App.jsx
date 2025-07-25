c7f45543bddda23c7bfe533be509b7c88a8c304a
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-07-25 23:59:23 +0530

    refact: added protectedRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 import CitizenLayout from "layouts/citizen";
 import AdminLayout from "layouts/admin";
@@ -7,17 +7,44 @@ import AuthLayout from "layouts/auth";
 import HomePage from "views/HomePage";
 import StaffLayout from "layouts/staff";
 import ScrollToTop from "views/citizen/news/components/ScrollToTop";
+import ProtectedRoute from "utils/ProtectedRoutes";
 
 const App = () => {
+  const role = localStorage.getItem("role");
+  const navigate = useNavigate();
+  if (role == null || role == undefined) {
+    navigate("/auth/signin");
+  }
   return (
     <>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="citizen/*" element={<CitizenLayout />} />
+        <Route
+          path="citizen/*"
+          element={
+            <ProtectedRoute requiredRole="user" Role={role}>
+              <CitizenLayout />
+            </ProtectedRoute>
+          }
+        />
         <Route path="auth/*" element={<AuthLayout />} />
-        <Route path="admin/*" element={<AdminLayout />} />
-        <Route path="staff/*" element={<StaffLayout />} />
+        <Route
+          path="admin/*"
+          element={
+            <ProtectedRoute requiredRole="admin" Role={role}>
+              <AdminLayout />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          path="staff/*"
+          element={
+            <ProtectedRoute requiredRole="staff" Role={role}>
+              <StaffLayout />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/*" element={<CitizenLayout />} />
       </Routes>
     </>
