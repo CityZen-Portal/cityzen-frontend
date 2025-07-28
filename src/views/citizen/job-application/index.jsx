9cccf9ff51d882b60047cbfbf6f44b81e62dbdfb
bharathloganathan <bharathloganathan2714@gmail.com>
2025-07-28 20:09:54 +0530

    Job application form

--- src/views/citizen/job-application/index.jsx
+++ src/views/citizen/job-application/index.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+// src/views/citizen/job-application/index.jsx
+
+import React, { useState } from 'react';
+import JobApplicationForm from './pages/JobApplicationForm';
+import JobApplicationResponseView from './pages/JobApplicationResponseView';
+import JobApplicationEdit from './pages/JobApplicationEdit';
 
 const JobApplicationPage = () => {
-  return (
-    <div>JobApplicationPage</div>
-  )
-}
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formState, setFormState] = useState({
+    formData: null,
+    location: null,
+    experiences: null,
+  });
 
-export default JobApplicationPage
+  const handleFormSubmit = (formData, location, experiences) => {
+    setFormState({ formData, location, experiences });
+    setIsSubmitted(true);
+  };
+
+  const handleEdit = () => {
+    setIsSubmitted(false);
+  };
+
+  return (
+    <div>
+      {/* {!isSubmitted ? (
+        <JobApplicationForm
+          onFormSubmit={handleFormSubmit}
+          initialData={formState}
+        />
+      ) : (
+        <JobApplicationResponseView
+          formData={formState.formData}
+          location={formState.location}
+          experiences={formState.experiences}
+          onEdit={handleEdit}
+        />
+      )} */}
+      <JobApplicationEdit />
+    </div>
+  );
+};
+
+export default JobApplicationPage;
