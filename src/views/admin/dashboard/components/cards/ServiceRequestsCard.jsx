657cbe5eb671e0d83631be663958efd5231ee2b4
jeevi4 <pjeevitha370@gmail.com>
2025-07-31 21:10:35 +0530

    fix-admin-dashboard-count

--- src/views/admin/dashboard/components/cards/ServiceRequestsCard.jsx
+++ src/views/admin/dashboard/components/cards/ServiceRequestsCard.jsx
@@ -1,4 +1,18 @@
+import {useEffect,useState} from "react"; 
 import DashboardCard from "../DashboardCard";
+import axios from "axios";
+
+const url = process.env.REACT_APP_API_UTILITY_URL;
 export default function ServiceRequestsCard() {
-  return <DashboardCard title="Service Requests" />;
+  const[num, setNum] = useState(0);
+  useEffect(()=> {
+    const fetchData = async() =>{
+      const response = await axios.get(`${url}/api/admin/service/task`);
+      console.log(response.serviceCount);
+      setNum(response.data.serviceCount);
+    };
+    fetchData();
+  },[]);
+
+  return <DashboardCard title="Service Requests" count ={num}/>;
 }
--- src/views/admin/dashboard/components/cards/StaffTasksCard.jsx
+++ src/views/admin/dashboard/components/cards/StaffTasksCard.jsx
@@ -1,4 +1,19 @@
+import {useEffect, useState} from "react";
 import DashboardCard from "../DashboardCard";
+import axios from "axios";
+
+const url = process.env.REACT_APP_API_UTILITY_URL;
 export default function StaffTasksCard() {
-  return <DashboardCard title="Staff Tasks" />;
+  const [num, setNum] = useState(0);
+  useEffect(() => {
+    const fetchData = async () =>{
+      const response = await axios.get(`${url}/api/admin/service/task`);
+
+      console.log(response.data.stafftaskCount);
+      setNum(response.data.stafftaskCount);
+    };
+    fetchData();
+  },[]);
+
+  return <DashboardCard title="Staff Tasks" count ={num} />;
 }
